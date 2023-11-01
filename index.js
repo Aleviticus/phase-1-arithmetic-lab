8975d53af894cdb07e7fe278c323e5602409e012
Aaron Levy <aaronlevypc21@gmail.com>
2023-10-31 23:41:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2)
+console.log(multiply)
 
 // Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 8
+let num4 = 2
+const mod = (num3 / num4)
+console.log(mod);
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
